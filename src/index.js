b49c9f8f2c845345fb047a928fe7221bbf1d2246
pavel <pavelv87@gmail.com>
2019-10-11 19:38:26 +0600

    test Codeclimate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const testGame = () => {
+const testGame =()=> {
     console.log('Welcome to the Brain Games!');
     let name = readlineSync.question('May I have your name? ');
     console.log('Hello ' + name + '!');
