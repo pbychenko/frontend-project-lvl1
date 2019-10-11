10e803b48514ed7fc842f3bbc9eab8106cc519b6
pavel <pavelv87@gmail.com>
2019-10-11 20:26:11 +0600

    test Travis

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const testGame = () => {
     console.log('Welcome to the Brain Games!');
-    let name = readlineSync.question('May I have your name? ');
+    let name= readlineSync.question('May I have your name? ')
     console.log('Hello ' + name + '!');
 }
 
