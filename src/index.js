8d1b1d8e2f251b7dff01964de85a3ecce981a7da
pavel <pavelv87@gmail.com>
2019-10-16 16:49:03 +0600

    architecture changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const startGame = () => {
+export const common = (gameName, levelFlow) => {
+    let attemptCount = 3;
     console.log('Welcome to the Brain Games!');
-    let name = readlineSync.question('May I have your name? ');
-    console.log('Hello ' + name + '!');
-}
+    console.log(gameName);
 
-export default startGame;
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello ' + name + '!' + '\n');
+
+    while (attemptCount > 0) {
+        let {answer, correctAnswer} = levelFlow();
+
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+            attemptCount -= 1;
+            continue;
+        }
+        
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+        console.log(`Let's try again, ${name}!`);
+        break;
+    }
+
+    if (attemptCount === 0) {
+        console.log(`Congratulations, ${name}!`);
+    }
+};
+
