55d6b123a25ef5b33cf7c9c8b395785a271be68a
pavel <pavelv87@gmail.com>
2019-10-24 10:41:12 +0600

    Review fixes part 4

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,12 +2,12 @@ import { getRandomInt, max, min } from '../utils';
 import runApp from '..';
 
 const gameDescription = 'What number is missing in the progression?';
-const progressionLength = 5;
+const progressionLength = 10;
 
-const getProgression = (start, step) => {
+const getProgression = (start, step, length) => {
   const progression = [];
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression[i] = start + step * i;
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
   }
   return progression;
 };
@@ -21,7 +21,7 @@ const getRoundProgression = (list, position) => {
 const generateRoundData = () => {
   const start = getRandomInt(min, max);
   const step = getRandomInt(min, max);
-  const progression = getProgression(start, step);
+  const progression = getProgression(start, step, progressionLength);
   const questionPosition = getRandomInt(0, progression.length - 1);
   const question = `${getRoundProgression(progression, questionPosition)}`;
   const correctAnswer = String(progression[questionPosition]);
